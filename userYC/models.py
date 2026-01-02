6a6c8f736b7fdf95ca575c04b4de84ba75cd2c7b
jessec01 <jegro17@gmail.com>
2026-01-02 10:17:24 -0400

    feat: a timestamp was added for each user created

--- userYC/models.py
+++ userYC/models.py
@@ -3,9 +3,10 @@
 #se instala y define el paquete phonenumber_field para manejar numeros telefonicos 
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
-
+from django.db import models
 class UserYogaCenter(AbstractUser):
     phone = PhoneNumberField(blank=True, help_text='Contact phone number')
+    datatime_created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name = "UserYC"
