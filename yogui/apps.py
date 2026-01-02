a5dec80ab69179febe4aafdfd38ce58949eda691
jessec01 <jegro17@gmail.com>
2026-01-02 10:44:33 -0400

    feat: new app created for managing the yogi user profile

---
+++ yogui/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YoguiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'yogui'
---
+++ yogui/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from userYC.models import UserYogaCenter
+class YoguiProfile(models.Model):
+    user = models.OneToOneField(UserYogaCenter, on_delete=models.CASCADE)
+    description_personal = models.TextField(blank=True)
+    experience_level = models.CharField(max_length=50, blank=True)
+    url_profile_picture = models.ImageField()
+    is_active = models.BooleanField(default=True)
+    point_accumulated = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
